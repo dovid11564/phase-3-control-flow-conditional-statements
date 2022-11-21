53c149c27d088f77d3198ac4b963c2ae8f3b39fb
Dovid Edelkopf <dovidzedelkopf@gmail.com>
2022-11-20 19:04:47 -0500

    commit tree f37b953d5721e9bc826dc6a008c6f3e97d0bcfe4

--- control_flow.rb
+++ control_flow.rb
@@ -1,5 +1,12 @@
 def admin_login(username, password)
   # your code here
+
+  if username = "admin".upcase
+    "Access granted"
+  
+  if username != "admin".upcase
+    "Access denied"
+  end
 end
 
 def hows_the_weather(temperature)
@@ -13,4 +20,3 @@ end
 def calculator(operation, num1, num2)
   # your code here
 end
-
